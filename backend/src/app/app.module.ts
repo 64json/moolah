b171ac267ab81830820a4188e71f1522c6153cbd
Jinseo Park <jinseo@google.com>
2021-07-02 19:23:25 -0400

    Set up mongodb and directory structure

---
+++ backend/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from '../user/user.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost/moolah'),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+}
---
+++ backend/src/user/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './user.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {
+  }
+
+  async create(createCatDto: CreateUserDto): Promise<User> {
+    const createdCat = new this.userModel(createCatDto);
+    return createdCat.save();
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+}
