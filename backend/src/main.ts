cb425b1bf74c28605ec4bcc3262651cf3254ed6b
Jinseo Park <jinseo@google.com>
2021-07-08 16:19:04 -0400

    Remove localhost from cors origin in production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://jasonpark.me'],
+    origin: [process.env.NODE_ENV === 'production' ? 'https://jasonpark.me' : 'http://localhost:3000'],
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 8000);
